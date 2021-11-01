c85f3575e8b6bb0f3be9cd56e1300366e17c5786
Marco <marco@task.media>
2021-11-01 15:34:28 +0100

    check if years in championship follow each other

--- nuScrape.go
+++ nuScrape.go
@@ -22,10 +22,10 @@ func main() {
 }
 
 func getGesamtspielplanChampionshipGroup(c *gin.Context) {
-	year := c.Param("year")
-	if !validateYear(year) {
-		log.WithField("year", year).Warning("year not matching with regex")
-		c.String(http.StatusBadRequest, "year pattern not valid - please use YYYYYY")
+	year, err := getYear(c.Param("year"))
+	if err != nil {
+		log.WithField("year", year).Warning(err)
+		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
 
@@ -46,8 +46,30 @@ func getGesamtspielplanChampionshipGroup(c *gin.Context) {
 	c.String(http.StatusOK, "not yet implemented")
 }
 
-func validateYear(y string) bool {
-	return regexp.MustCompile(`\d{6}`).MatchString(y)
+// Get and validate year
+// Checks if the year pattern is matching 6 digits and is correct
+// YYYYYY - e.g. 202122 for 2021/22
+func getYear(yearString string) (int, error) {
+	if !regexp.MustCompile(`\d{6}`).MatchString(yearString) {
+		return -1, errors.New("year pattern not valid (expected 6 digits)")
+	}
+
+	y, err := strconv.Atoi(yearString)
+	if err != nil {
+		return -1, errors.New("year not an integer")
+	}
+
+	// check if years match to each other
+	// valid: e.g. 202122, 199900
+	// not valid: e.g. 202123
+	hr, _ := strconv.Atoi(yearString[2:4])
+	rr, _ := strconv.Atoi(yearString[4:])
+
+	if !(((hr + 1) == rr) || (hr == 99 && rr == 0)) {
+		return -1, errors.New("year patter not valid (years must follow each other)")
+	}
+
+	return y, nil
 }
 
 // Check if the championship abbreviation is valid
