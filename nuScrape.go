0c887d858625a25c8abac221edd95033df3e26c8
Marco <marco@task.media>
2021-11-01 13:39:04 +0100

    use gin-gonic to create API

--- nuScrape.go
+++ nuScrape.go
@@ -1,9 +1,23 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 
 	log.Info("Starting nuScrape")
+
+	router := gin.Default()
+	router.GET("/gesamtspielplan/:championship/:group", getGesamtspielplanChampionshipGroup)
+
+	router.Run("localhost:8080")
+}
+
+func getGesamtspielplanChampionshipGroup(c *gin.Context) {
+	c.String(http.StatusOK, "not yet implemented")
 }
