c14a18423b9151bce6d7daeb5bc65880e2f6ffde
Marco <marco@task.media>
2021-11-02 23:15:45 +0100

    update url test to use assert; added documentation

--- pkg/scrape/url_test.go
+++ pkg/scrape/url_test.go
@@ -3,11 +3,13 @@ package scrape
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"github.com/taskmedia/nuScrape/pkg/sport/group"
 	"github.com/taskmedia/nuScrape/pkg/sport/season"
 )
 
+// Test func GenerateUrlGesamtspielplan
+// This test will check if a generated URL will be correctly generated
 func TestGenerateUrlGesamtspielplan(t *testing.T) {
 	expectedUrlString := "https://bhv-handball.liga.nu/cgi-bin/WebObjects/nuLigaHBDE.woa/wa/groupPage?championship=AV+2021%2F22&displayDetail=meetings&displayTyp=gesamt&group=281103"
 
@@ -19,5 +21,5 @@ func TestGenerateUrlGesamtspielplan(t *testing.T) {
 
 	u := generateUrlGesamtspielplan(s, c, g)
 
-	require.Equal(t, expectedUrlString, u.String(), "The two URLs should match.")
+	assert.Equal(t, expectedUrlString, u.String(), "The two URLs should match")
 }
