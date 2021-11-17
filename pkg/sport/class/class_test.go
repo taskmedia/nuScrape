1d440dbfb6e63619fd3c3a8a50951ce30dea12bf
Marco <marco@task.media>
2021-11-17 19:38:11 +0100

    add class testing

---
+++ pkg/sport/class/class_test.go
@@ -0,0 +1,20 @@
+package class
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test func TestGetAbbreviation if it returns the correct abbreviation
+func TestGetAbbreviation(t *testing.T) {
+	assert.Equal(t, "BL", GetAbbreviation("Bayernliga"), "Expected abbreviation was not correct for Bayernliga")
+	assert.Equal(t, "BOL", GetAbbreviation("Bezirksoberliga"), "Expected abbreviation was not correct for Bezirksoberliga")
+	assert.Equal(t, "ÜBOL", GetAbbreviation("ÜBOL"), "Expected abbreviation was not correct for ÜBOL")
+}
+
+// Test func TestGetFullname if it returns the correct complete name of a class
+func TestGetFullname(t *testing.T) {
+	assert.Equal(t, "Landesliga", GetFullname("LL"), "Expected fullname was not correct for LL")
+	// assert.Equal(t, "Bezirksklasse", GetFullname("BZK"), "Expected fullname was not correct for BZK") // not possible because duplicate entries
+}
