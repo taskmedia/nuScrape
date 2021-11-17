c1e034acc652cc074284e9191569df0ddfb6b6c5
Marco <marco@task.media>
2021-11-17 20:11:19 +0100

    change class test warning message to match with relay test

--- pkg/sport/class/class_test.go
+++ pkg/sport/class/class_test.go
@@ -8,13 +8,13 @@ import (
 
 // Test func TestGetAbbreviation if it returns the correct abbreviation
 func TestGetAbbreviation(t *testing.T) {
-	assert.Equal(t, "BL", GetAbbreviation("Bayernliga"), "Expected abbreviation was not correct for Bayernliga")
-	assert.Equal(t, "BOL", GetAbbreviation("Bezirksoberliga"), "Expected abbreviation was not correct for Bezirksoberliga")
-	assert.Equal(t, "ÜBOL", GetAbbreviation("ÜBOL"), "Expected abbreviation was not correct for ÜBOL")
+	assert.Equal(t, "BL", GetAbbreviation("Bayernliga"), "Expected class abbreviation was not correct for Bayernliga")
+	assert.Equal(t, "BOL", GetAbbreviation("Bezirksoberliga"), "Expected class abbreviation was not correct for Bezirksoberliga")
+	assert.Equal(t, "ÜBOL", GetAbbreviation("ÜBOL"), "Expected class abbreviation was not correct for ÜBOL")
 }
 
 // Test func TestGetFullname if it returns the correct complete name of a class
 func TestGetFullname(t *testing.T) {
-	assert.Equal(t, "Landesliga", GetFullname("LL"), "Expected fullname was not correct for LL")
+	assert.Equal(t, "Landesliga", GetFullname("LL"), "Expected class fullname was not correct for LL")
 	// assert.Equal(t, "Bezirksklasse", GetFullname("BZK"), "Expected fullname was not correct for BZK") // not possible because duplicate entries
 }
