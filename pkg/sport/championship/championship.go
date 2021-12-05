4115440ffeaa0d8c2c67b5df287f2398d6675d83
Marco <marco@task.media>
2021-12-05 15:50:00 +0100

    add championship struct
    closes #59

--- pkg/sport/championship/championship.go
+++ pkg/sport/championship/championship.go
@@ -1,24 +1,96 @@
 package championship
 
-// Allowed championship for BHV
-var championshipAbb = [...]string{
-	"BHV",
-	"UF",
-	"OF",
-	"MF",
-	"OS",
-	"SW",
-	"AB",
-	"AV",
-	"OB",
+import (
+	"errors"
+	"reflect"
+	"strings"
+)
+
+type Championship string
+
+const (
+	BHV = Championship("Bayerischer Handball-Verband")
+	UF  = Championship("Unterfranken")
+	OF  = Championship("Oberfranken")
+	MF  = Championship("Mittelfranken")
+	OS  = Championship("Ostbayern")
+	SW  = Championship("Schwaben")
+	AB  = Championship("Altbayern")
+	AV  = Championship("Alpenvorland")
+	OB  = Championship("Oberbayern")
+)
+
+// func GetAbbreviation returns short name of a championship
+func (c Championship) GetAbbreviation() string {
+	switch c {
+	case BHV:
+		return "BHV"
+	case UF:
+		return "UF"
+	case OF:
+		return "OF"
+	case MF:
+		return "MF"
+	case OS:
+		return "OS"
+	case SW:
+		return "SW"
+	case AB:
+		return "AB"
+	case AV:
+		return "AV"
+	case OB:
+		return "OB"
+	default:
+		return "invalid championship (abbreviation)"
+	}
 }
 
-// ValidateChampionshipAbb checks if the given championship abbreviation is valid
-func ValidateChampionshipAbb(c string) bool {
-	for _, ca := range championshipAbb {
-		if ca == c {
-			return true
-		}
+// func GetName returns the full name of a championship
+func (c Championship) GetName() string {
+	return reflect.ValueOf(c).String()
+}
+
+// func ParseAbbreviation converts a given abbreviation to a championship
+func ParseAbbreviation(s string) (Championship, error) {
+	switch strings.ToUpper(s) {
+	case
+		BHV.GetAbbreviation():
+		return BHV, nil
+
+	case
+		UF.GetAbbreviation():
+		return UF, nil
+
+	case
+		OF.GetAbbreviation():
+		return OF, nil
+
+	case
+		MF.GetAbbreviation():
+		return MF, nil
+
+	case
+		OS.GetAbbreviation():
+		return OS, nil
+
+	case
+		SW.GetAbbreviation():
+		return SW, nil
+
+	case
+		AB.GetAbbreviation():
+		return AB, nil
+
+	case
+		AV.GetAbbreviation():
+		return AV, nil
+
+	case
+		OB.GetAbbreviation():
+		return OB, nil
+
+	default:
+		return "error", errors.New("could not parse class type (unknown)")
 	}
-	return false
 }
--- pkg/sport/championship/championship_test.go
+++ pkg/sport/championship/championship_test.go
@@ -6,17 +6,49 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Test func ValidateChampionshipAbb
-// This test will test different abbreviations and check the return value
-func TestValidateChampionshipAbb(t *testing.T) {
-	expectedSuccess := []string{"BHV", "AV", "MF"}
-	expectedFailure := []string{"XX", "YZ", "Bhv", "av"}
-
-	for _, test := range expectedSuccess {
-		assert.Equal(t, true, ValidateChampionshipAbb(test), "Expected abbreviation (%s) should be valid for championship", test)
+// Test func GetAbbreviation if it returns the correct championship
+func TestParse(t *testing.T) {
+	testChampionship := map[string]Championship{
+		"bhv": BHV,
+		"UF":  UF,
+		"Of":  OF,
+		"mF":  MF,
+		"OS":  OS,
+		"SW":  SW,
+		"AB":  AB,
+		"AV":  AV,
+		"OB":  OB,
 	}
 
-	for _, test := range expectedFailure {
-		assert.Equal(t, false, ValidateChampionshipAbb(test), "Expected abbreviation (%s) should be NOT valid for championship", test)
+	for check, expected := range testChampionship {
+		c, err := ParseAbbreviation(check)
+		assert.Equal(t, expected, c, "expected other constant from parsing championship from value %s", check)
+		assert.Equal(t, nil, err, "expected no error from parsing championship from value %s", check)
 	}
+}
+
+// Test func GetAbbreviation if it returns the correct abbreviation of a championship
+func TestGetAbbreviation(t *testing.T) {
+	assert.Equal(t, "BHV", BHV.GetAbbreviation(), "expected other abbreviation from championship BHV")
+	assert.Equal(t, "UF", UF.GetAbbreviation(), "expected other abbreviation from championship UF")
+	assert.Equal(t, "OF", OF.GetAbbreviation(), "expected other abbreviation from championship OF")
+	assert.Equal(t, "MF", MF.GetAbbreviation(), "expected other abbreviation from championship MF")
+	assert.Equal(t, "OS", OS.GetAbbreviation(), "expected other abbreviation from championship OS")
+	assert.Equal(t, "SW", SW.GetAbbreviation(), "expected other abbreviation from championship SW")
+	assert.Equal(t, "AB", AB.GetAbbreviation(), "expected other abbreviation from championship AB")
+	assert.Equal(t, "AV", AV.GetAbbreviation(), "expected other abbreviation from championship AV")
+	assert.Equal(t, "OB", OB.GetAbbreviation(), "expected other abbreviation from championship OB")
+}
+
+// Test func GetName if it returns the correct full name of a championship
+func TestGetName(t *testing.T) {
+	assert.Equal(t, "Bayerischer Handball-Verband", BHV.GetName(), "expected other full name from championship BHV")
+	assert.Equal(t, "Unterfranken", UF.GetName(), "expected other full name from championship UF")
+	assert.Equal(t, "Oberfranken", OF.GetName(), "expected other full name from championship OF")
+	assert.Equal(t, "Mittelfranken", MF.GetName(), "expected other full name from championship MF")
+	assert.Equal(t, "Ostbayern", OS.GetName(), "expected other full name from championship OS")
+	assert.Equal(t, "Schwaben", SW.GetName(), "expected other full name from championship SW")
+	assert.Equal(t, "Altbayern", AB.GetName(), "expected other full name from championship AB")
+	assert.Equal(t, "Alpenvorland", AV.GetName(), "expected other full name from championship AV")
+	assert.Equal(t, "Oberbayern", OB.GetName(), "expected other full name from championship OB")
 }
